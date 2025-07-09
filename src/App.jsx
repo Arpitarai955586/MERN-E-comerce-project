210f261a79d9acc934a7bfa00427c4031377d768
Your Name <your@email.com>
2025-07-09 13:17:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import Navigation from './customer/component/navigation/Navigation'
+
+function App() {
+  
+  return (
+    <div>
+    <Navigation/>
+    </div>
+  )
+}
+
+export default App
